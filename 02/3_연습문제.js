78a3d9ce406e3bca46b9552f423de6d2ec0e92d4
popomon <popomonclub@gmail.com>
2024-07-16 11:44:04 +0900

    doc: Javascript 2강 강의내용 정리

---
+++ 02/3_연습문제.js
@@ -0,0 +1,33 @@
+// 변수 타입 확인
+
+// 1. 다음 변수들의 타입을 확인하여 콘솔에 출력하세요.
+let num = 42;
+let name = 'John';
+let isTrue = true;
+let person = {name: 'Alice', age: 30};
+let fruits = ['apple', 'banana', 'orange'];
+
+console.log('1.다음 변수들의 타입을 확인하여 콘솔에 출력하세요.');
+console.log('typeof num:', typeof num);
+console.log('typeof name:', typeof name);
+console.log('typeof isTrue:', typeof isTrue);
+console.log('typeof person:', typeof person);
+console.log('typeof fruits:', typeof fruits);
+console.log('');
+
+// 2. 다음 주석 친 부분에 주석 왼쪽에 있는 데이터의 자료형을 적어주세요.
+let length = 16; // 'number'
+let lastName = 'Brown'; // 'string'
+let score = [10, 20]; // 'object'
+let x = {
+	firstName: 'Nick',
+	lastName: 'Doe',
+}; // 'object'
+
+console.log(
+	'2. 다음 주석 친 부분에 주석 왼쪽에 있는 데이터의 자료형을 적어주세요.'
+);
+console.log('typeof length:', typeof length);
+console.log('typeof lastName:', typeof lastName);
+console.log('typeof score:', typeof score);
+console.log('typeof x:', typeof x);
